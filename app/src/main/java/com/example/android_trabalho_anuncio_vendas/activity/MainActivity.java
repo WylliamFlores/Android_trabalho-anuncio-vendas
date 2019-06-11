5fbfef7639e64e5730fd0bdd051c8815087df9b9
WylliamFlores <wylliammf@gmail.com>
2019-06-11 20:42:15 -0300

    cadastrar usuario

--- app/src/main/java/com/example/android_trabalho_anuncio_vendas/activity/MainActivity.java
+++ app/src/main/java/com/example/android_trabalho_anuncio_vendas/activity/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.android_trabalho_anuncio_vendas.activity;
 
+import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.android_trabalho_anuncio_vendas.R;
 import com.example.android_trabalho_anuncio_vendas.helper.ConfiguracaoFirebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +35,50 @@ public class MainActivity extends AppCompatActivity {
 
         componentesLogin();
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = cadastroEmail.getText().toString();
+                String senha = cadastroSenha.getText().toString();
+
+                if (!email.isEmpty() ){
+                    if (!senha.isEmpty() ){
+                        autenticacao.createUserWithEmailAndPassword(email, senha)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(MainActivity.this,"Cadastro realizado com sucesso =)", Toast.LENGTH_SHORT).show();
+                                }else{
+                                    String erroExcecao ="";
+
+                                    try{
+                                        throw task.getException();
+                                    }catch (FirebaseAuthWeakPasswordException e){
+                                        erroExcecao = "Digite uma senha mais forte!";
+                                    }catch (FirebaseAuthInvalidCredentialsException e){
+                                        erroExcecao = "Digite um e-mail válido";
+                                    }catch (FirebaseAuthUserCollisionException e){
+                                        erroExcecao = "Esta conta já existe";
+                                    } catch (Exception e) {
+                                        erroExcecao = " ao cadastrar usuário: "  + e.getMessage();
+                                        e.printStackTrace();
+                                    }
+                                    Toast.makeText(MainActivity.this, "Erro: " + erroExcecao, Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+                    }else {
+                        Toast.makeText(MainActivity.this, "Preencha o campo Senha!", Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+                    Toast.makeText(MainActivity.this, "Preencha o campo E-mail!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
     }
 
