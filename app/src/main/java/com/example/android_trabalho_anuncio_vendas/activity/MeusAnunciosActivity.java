d09e96e5351bee27fac11f9a807c55098774630f
WylliamFlores <wylliammf@gmail.com>
2019-06-17 23:19:44 -0300

    Listar meus anuncios

--- app/src/main/java/com/example/android_trabalho_anuncio_vendas/activity/MeusAnunciosActivity.java
+++ app/src/main/java/com/example/android_trabalho_anuncio_vendas/activity/MeusAnunciosActivity.java
@@ -5,17 +5,43 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.example.android_trabalho_anuncio_vendas.R;
+import com.example.android_trabalho_anuncio_vendas.adapter.AdapterAnuncios;
+import com.example.android_trabalho_anuncio_vendas.helper.ConfiguracaoFirebase;
+import com.example.android_trabalho_anuncio_vendas.model.Anuncio;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class MeusAnunciosActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerAnuncios;
+    private List<Anuncio> anuncios = new ArrayList<>();
+    private AdapterAnuncios adapterAnuncios;
+    private DatabaseReference anuncioUsuarioReferencia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meus_anuncios);
+
+        //Configurações iniciais
+        anuncioUsuarioReferencia = ConfiguracaoFirebase.getFirebase()
+                .child("meus_anuncios")
+                .child( ConfiguracaoFirebase.getIdUsuario() );
+
+        Componentes();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -27,6 +53,44 @@ public class MeusAnunciosActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        //Configurando RecyclerView
+        recyclerAnuncios.setLayoutManager(new LinearLayoutManager(this));
+        recyclerAnuncios.setHasFixedSize(true);
+        adapterAnuncios = new AdapterAnuncios(anuncios, this);
+        recyclerAnuncios.setAdapter( adapterAnuncios );
+
+        //Recupera anúncios
+        recuperarAnuncios();
+    }
+
+    public void Componentes (){
+        recyclerAnuncios = findViewById(R.id.recyclerAnuncios);
+    }
+
+    private void recuperarAnuncios(){
+
+        anuncioUsuarioReferencia.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                anuncios.clear();
+                for ( DataSnapshot dsnapshot : dataSnapshot.getChildren() ){
+                    anuncios.add( dsnapshot.getValue(Anuncio.class) );
+                }
+
+                Collections.reverse( anuncios );
+                adapterAnuncios.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
 }
